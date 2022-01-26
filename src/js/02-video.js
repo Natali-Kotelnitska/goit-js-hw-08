ae99383366425dcdc613dc586aacf732c2d607ea
Nataliia Kotelnitska <nkotelnicka@gmail.com>
2022-01-27 01:41:01 +0200

    finish Task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,34 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+function onTimeUpdate(data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}
+
+const pausedTime = localStorage.getItem('videoplayer-current-time');
+
+player
+  .setCurrentTime(pausedTime)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
+
+// player.getVideoTitle().then(function (title) {
+//   console.log('title:', title);
+// });
