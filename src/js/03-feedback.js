5c655bf9e9cf203b72e4fcddf6232a0abda2c31e
Nataliia Kotelnitska <nkotelnicka@gmail.com>
2022-01-27 16:59:41 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+
+formEl.addEventListener('submit', onFormSubmit);
+formEl.addEventListener('input', throttle(onTextareaInput, 500));
+
+populateFormInputs();
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  formEl.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function onTextareaInput(e) {
+  formData[e.target.name] = e.target.value;
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function populateFormInputs() {
+  const savedMessage = localStorage.getItem(STORAGE_KEY);
+  const parsedMessage = JSON.parse(savedMessage);
+
+  if (savedMessage) {
+    formEl.email.value = parsedMessage.email;
+    formEl.message.value = parsedMessage.message;
+  }
+
+  console.log(parsedMessage);
+}
